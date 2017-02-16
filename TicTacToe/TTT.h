bc05b65876709b04d5d543f17c5e0e52528b641b
Rui Alves <up201606746@fe.up.pt>
2017-02-16 22:11:38 +0000

    Create TTT.h

---
+++ TicTacToe/TTT.h
@@ -0,0 +1,9 @@
+#ifndef TTT_H_INCLUDED
+#define TTT_H_INCLUDED
+
+void draw (char board[3][3]);
+void pause ();
+void gameplay (int n, char board[3][3]);
+void logics (int n, int x, int y, char board[3][3]);
+
+#endif // TTT_H_INCLUDED
